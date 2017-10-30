f9d87558f7fc16df2e8389997704bfa195758069
emilbikj <emil.bikjanov@gmail.com>
2017-10-30 15:07:36 +0200

    first commit

---
+++ src/main/java/lv/tele2ssc/easybuy/EasyBuyApplication.java
@@ -0,0 +1,12 @@
+package lv.tele2ssc.easybuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBuyApplication.class, args);
+	}
+}
