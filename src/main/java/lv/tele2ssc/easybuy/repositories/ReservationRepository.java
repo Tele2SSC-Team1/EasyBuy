a1b2bb416d659175221555ffcc0f9bc39a195ee4
emilbikj <emil.bikjanov@gmail.com>
2017-11-30 16:30:10 +0200

    fix

--- src/main/java/lv/tele2ssc/easybuy/repositories/ReservationRepository.java
+++ src/main/java/lv/tele2ssc/easybuy/repositories/ReservationRepository.java
@@ -9,13 +9,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
-        
-    List<Reservation> findAllByClient(User client);    
-    
+
+    @Query("SELECT r FROM Reservation r WHERE r.client = ?1 order by r.status asc")
+    List<Reservation> findAllByClient(User client);
+
     @Query("SELECT r FROM Reservation r WHERE "
             + " r.status = 'CLOSED' AND r.client = ?1")
     List<Reservation> findClosedReservations(User user);
-    
+
     @Query("SELECT r FROM Reservation r WHERE "
             + " (r.status = 'PROGRESS' OR r.status='APPROVED' OR r.status='DECLINED') AND r.client = ?1")
     List<Reservation> findNotClosedReservations(User user);
